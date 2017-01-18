39405450b8c01089eaf196d02a3c124f231418b3
AeonFr <frankno.94@gmail.com>
2017-01-17 21:04:47 -0300

    cambió la forma de manejar eventos de ratón

--- element_td.js
+++ element_td.js
@@ -13,7 +13,7 @@ function animate(func) {
 	}
 	
 	function frame(time) {
-		var frame_time = time - last;
+		var frame_time = (time - last) / 1000;
 		last = time;
 		if(func(frame_time) !== false) {
 			requestAnimationFrame(frame);
@@ -25,6 +25,22 @@ var mouse = {x: 0, y: 0},
 	click = false,
 	escape = false;
 
+function draw_entities(entities) {
+	var i, len = entities.length,
+		hover, m = mouse, c = click;
+
+	for(i = 0; i < len; i++) {
+		with(entities[i]) {
+			hover = m.x > x && m.y > y &&
+					m.x < x + width && m.y < y + height;
+			draw(x, y, hover);
+			if(hover && c && onclick) {
+				onclick(x, y);
+			}
+		}
+	}
+}
+/*
 function trigger_events(listeners) {
 	var i = 0, len = listeners.length;
 	
@@ -49,7 +65,7 @@ function trigger_events(listeners) {
 		}
 	}
 }
-
+*/
 window.onmousemove = function(event) {
 	var rect = background_canvas.getBoundingClientRect();
 	mouse.x = (event.clientX - rect.left) / scale;
@@ -236,6 +252,14 @@ function arrow(cx, direction, dest, size, color, line) {
 	cx.stroke();
 }
 
+function circle(cx, corner, radius, color) {
+	if(color) cx.fillStyle = color;
+	cx.beginPath();
+	cx.arc(corner[0] * scale + radius, corner[1] * scale + radius,
+		   radius, 0, 6.28);
+	cx.fill();
+}
+
 /*=====  End of CANVAS, MAPA Y FONDO  ======*/
 
 
@@ -255,73 +279,54 @@ var state,
 =            MENÚ PRINCIPAL            =
 ======================================*/
 
-menu = { listeners: [] };
+menu = { entities: [] };
 
 menu.load = function() {
 	state = 0;
-	clear(foreground);
-	clear(fx);
-	rect(foreground, [1.5, 2.5], 8, 5, "white");
 	foreground.textAlign = "left";
 	foreground.textBaseline = "top";
+};
+
+menu.frame = function() {
+	clear(foreground);
+	clear(fx);
+	//
+	rect(foreground, [1.5, 2.5], 8, 5, "white");
 	text(foreground, "ElementTD", [2, 3], "black", "bold 20");
 	line(foreground, [2, 3.75], [3, 3.75], "red", 2);
 	line(foreground, [3, 3.75], [6, 3.75], "#00ff00");
 	line(foreground, [6, 3.75], [9, 3.75], "#0000ff");
-	
-	//trigger all onblur events to draw all texts
-	menu.listeners.forEach(function(listener) {
-		listener.hovered = true;
-	});
-	
-	dot_marker(game.difficulty);
+	//
+	draw_entities(menu.entities);
+	//dot marking difficulty
+	rect(foreground, [2.75, 6 - 2/scale + .5 * game.difficulty], 3 / scale, 3 / scale);
 };
 
-menu.frame = function() {
-	trigger_events(menu.listeners);	
-};
-
-function embolden_text(string, position, height, onclick) {
+function menu_text_entity(string, dest, fontsize, onclick) {
 	return {
-		x: position[0], y: position[1],
-		w: text_width(foreground, string, height), h: height / scale,
-		
-		hovered: true, //trigger onblur event to draw text
-		
-		onhover: function() {
-			rect(foreground, [this.x, this.y], this.w, this.h, "white");
-			text(foreground, string, [this.x, this.y], "black", "bold " + height);
-		},
-		onblur: function() {
-			rect(foreground, [this.x, this.y], this.w, this.h, "white");
-			text(foreground, string, [this.x, this.y], "black", height);
+		x: dest[0], y: dest[1],
+		width: text_width(foreground, string, fontsize), height: fontsize / scale,
+		draw: function(x, y, hover) {
+			text(foreground, string, dest, "black", (hover ? "bold " : "") + fontsize);
 		},
 		onclick: onclick
 	};
 }
 
-function dot_marker(difficulty) {
-	rect(foreground, [2, 5.75], 1, 1.5, "white");
-	rect(foreground, [3 - .2, 5.75 + .5 * difficulty + .1], .1, .1, "black");
-}
-
-menu.listeners.push(
-	embolden_text("Instructions", [2, 4.25], 14, function() {
+menu.entities.push(
+	menu_text_entity("Instructions", [2, 4.25], 14, function() {
 		instructions.load();
 	}),
-	embolden_text("Play", [2, 5], 14, function() {
+	menu_text_entity("Play", [2, 5], 14, function() {
 		game.load();
 	}),
-	embolden_text("Easy", [3, 5.75], 12, function() {
-		dot_marker(0);
+	menu_text_entity("Easy", [3, 5.75], 12, function() {
 		game.difficulty = 0;
 	}),
-	embolden_text("Medium", [3, 6.25], 12, function() {
-		dot_marker(1);
+	menu_text_entity("Medium", [3, 6.25], 12, function() {
 		game.difficulty = 1;
 	}),
-	embolden_text("Hard", [3, 6.75], 12, function() {
-		dot_marker(2);
+	menu_text_entity("Hard", [3, 6.75], 12, function() {
 		game.difficulty = 2;
 	})
 );
@@ -333,16 +338,19 @@ menu.listeners.push(
 =            PANTALLA DE INSTRUCCIONES            =
 =================================================*/
 
-instructions = { listeners: [] };
+instructions = { entities: [] };
 
 instructions.load = function() {
 	state = 1;
-	clear(foreground);
-	clear(fx);
-	rect(foreground, [1.5, 2.5], 8, 7.1, "white");
 	foreground.textAlign = "left";
 	foreground.textBaseline = "top";
-	//arrow(foreground, -1, [2.15, 3.05], 16, "black", 2);
+};
+
+instructions.frame = function() {
+	clear(foreground);
+	clear(fx);
+	//
+	rect(foreground, [1.5, 2.5], 8, 7.1, "white");
 	text(foreground, "Instructions", [3, 3], "black", "bold 20");
 	line(foreground, [2, 3.75], [3, 3.75], "red");
 	line(foreground, [3, 3.75], [6, 3.75], "#00ff00");
@@ -356,35 +364,29 @@ instructions.load = function() {
 	text(foreground, "tral enemy is not the same as a", [2, 7.75]);
 	text(foreground, "neutral-fire enemy. Watch the ", [2, 8.25]);
 	text(foreground, "placement of your towers!", [2, 8.75]);
+	//
+	draw_entities(instructions.entities);
 };
 
-instructions.frame = function() {
-	trigger_events(instructions.listeners);
-};
-
-function navigation_arrow(direction, position, width, onclick) {
+function circular_button_entity(draw_content, dest, size, onclick) {
 	return {
-		//BACK TO MENU ARROW
-		x: position[0], y: position[1], w: width / scale, h: width / scale,
-		hovered: true, //we want to trigger the onblur event to draw it
-		
-		onhover: function() {
-			foreground.beginPath();
-			foreground.arc((this.x + this.w / 2) * scale, (this.y + this.h / 2) * scale,
-			               this.w * scale / 1.8, 0, 6.28);
-			foreground.fillStyle = "#BDBDFF";
-			foreground.fill();
-			arrow(foreground, direction, [this.x, this.y], this.w * scale, "black", 2);
-		},
-		onblur: function() {
-			rect(foreground, [this.x - 0.1, this.y - 0.1], this.w + 0.2, this.h + 0.2, "white");
-			arrow(foreground, direction, [this.x, this.y], this.w * scale, "black", 2);
+		x: dest[0], y: dest[1],
+		width: size / scale, height: size / scale,
+		draw: function(x, y, hover) {
+			if(hover) {
+				circle(foreground, [x, y], size / 2, "#bdbdff");
+			}
+			draw_content(x, y, hover);
 		},
 		onclick: onclick
 	};
 }
 
-instructions.listeners.push(navigation_arrow(-1, [2.15, 3.05], 16, menu.load));
+instructions.entities.push(
+	circular_button_entity(function(x, y) {
+		arrow(foreground, -1, [x, y], 16, "black", 2);
+	}, [2.15, 3.05], 16, menu.load)
+);
 
 /*=====  End of PANTALLA DE INSTRUCCIONES  ======*/
 
@@ -455,8 +457,6 @@ game = {
 
 game.load = function() {
 	state = 2;
-	clear(foreground);
-	clear(fx);
 	//(Re)set game defaults
 	with(game) {
 		gold_multiplier = 2 - difficulty * 0.5;
@@ -486,7 +486,6 @@ game.load = function() {
 		}
 	}
 	
-	
 	toolbar.load();
 };
 
@@ -502,10 +501,10 @@ game.animate = function(game, frame_time) {
 };
 
 game.render = function(game, frame_time) {
-		//we do not clear the toolbar
-	foreground.clearRect(0, 0, 11 * scale, map_height * scale);
+	clear(foreground);
 	clear(fx);
 	
+	//draw towers
 	var y, x, tower;
 	for(y = 0; y < map_height; y++) {
 		for(x = 0; x < map_width; x++) {
@@ -523,69 +522,48 @@ game.render = function(game, frame_time) {
 =======================================================*/
 
 toolbar = {
-	listeners: [],
-	buying_tower: undefined,
-	tower_info_hover: false, //used to clear buy-tower info display
-	tower_info_blur: false,
-	load_menu: false //used to delay the call to menu.load() to the end of the frame
+	entities: [],
+	buying_tower: undefined
 };
 
 toolbar.load = function() {
-	//Ensure to trigger all toolbar onblur events
-	toolbar.listeners.forEach(function(listener) {
-		listener.hovered = true;
-	});
-	toolbar.load_menu = toolbar.tower_info_hover = toolbar.tower_info_blur = false;
 	toolbar.buying_tower = undefined;
+};
+
+toolbar.frame = function() {
+	//white rect
+	rect(foreground, [11, 0], 4, 6, "white");
 	//Divisor line
 	line(foreground, [11, 0.8125], [11.5, 0.8125], "red");
 	line(foreground, [11.5, 0.8125], [13, 0.8125], "#00ff00");
 	line(foreground, [12.5, 0.8125], [15, 0.8125], "#0000ff");
-};
-
-toolbar.frame = function() {
 	
-	rect(foreground, [13, .15], 2, .5, "white");//clear
 	icon_text(foreground, [2, 0], game.lives, [13, .25], "black", "bold 10");
 	icon_text(foreground, [1, 0], game.gold, [14, .25]);
 	
-	trigger_events(toolbar.listeners);
-	
+	draw_entities(toolbar.entities);
+
 	var x = Math.floor(mouse.x),
-		y = Math.floor(mouse.y);
+		y = Math.floor(mouse.y),
+		buying_tower = toolbar.buying_tower;
 	
-	with(toolbar) {
-		//clear tower info display (it is done this way to fix a bug):
-		if(tower_info_blur && !tower_info_hover) {
-			rect(foreground, [14, 1], 1, 2, "white");
-		} else {
-			tower_info_hover = tower_info_blur = false;
-		}
-		
-		//buy tower
-		if(buying_tower !== undefined) {
-			if(game.towers[y] && game.towers[y][x] === undefined) {
-				//terrain available for towers
-				var range = towers[buying_tower].range;
-				fx.beginPath();
-				fx.arc((x + .5) * scale, (y + .5) * scale, (range + .5) * scale, 0, 6.28);
-				fx.fillStyle = "rgba(125,125,255,.2)";
-				fx.fill();
-				tile(fx, [4 + buying_tower, 1], [x, y - 1], 1, 2);
-				
-				if(click) {
-					//buy that tower, mr
-					var tower = Object.create(towers[buying_tower]);
-					game.gold -= tower.price;
-					game.towers[y][x] = tower;
-					buying_tower = undefined;
-				}
-			} else {
-				tile(fx, [4 + buying_tower, 1], [mouse.x - .5, mouse.y - 1.5], 1, 2);
-				if(click) {
-					buying_tower = undefined;
-				}
+	//buy tower
+	if(buying_tower !== undefined) {
+		if(game.towers[y] && game.towers[y][x] === undefined) {
+			//terrain available for towers, show range
+			var range = towers[buying_tower].range;
+			circle(fx, [x - range, y - range], (range + .5) * scale, "rgba(125, 125, 255, .2)");
+			tile(fx, [4 + buying_tower, 1], [x, y - 1], 1, 2);
+
+			if(click) {
+				//buy that tower, mr
+				var tower = Object.create(towers[buying_tower]);
+				game.gold -= tower.price;
+				game.towers[y][x] = tower;
+				toolbar.buying_tower = undefined;
 			}
+		} else {
+			tile(fx, [4 + buying_tower, 1], [mouse.x - .5, mouse.y - 1.5], 1, 2);
 		}
 	}
 	
@@ -597,73 +575,59 @@ toolbar.frame = function() {
 			game.selected_tower = undefined;
 		}
 	}
-	
-	if(toolbar.load_menu) {
-		menu.load();
-	}
 };
 
-function rounded_button(dest, width, height, draw, onclick) {
+function rounded_button_entity(draw_content, dest, width, height, round, onclick) {
 	return {
-		x: dest[0], y: dest[1], w: width, h: height,
-		hovered: true,
-		onhover: function() {
-			round_rect(foreground, [this.x, this.y], this.w, this.h, 6, "#BDBDFF");
-			draw(this.x, this.y, true);
-		},
-		onblur: function() {
-			rect(foreground, [this.x - .015, this.y - .015], this.w + .03, this.h + .03, "white");
-			draw(this.x, this.y, false);
+		x: dest[0], y: dest[1], width: width, height: height,
+		draw: function(x, y, hover) {
+			if(hover) {
+				round_rect(foreground, [x, y], width, height, round, "#bdbdff");
+			}
+			draw_content(x, y, hover);
 		},
 		onclick: onclick
 	};
 }
 
-function rect_button(dest, width, height, draw, onclick) {
+function rectangular_button(draw_content, dest, width, height, onclick) {
 	return {
-		x: dest[0], y: dest[1], w: width, h: height,
-		hovered: true,
-		onhover: function() {
-			rect(foreground, [this.x, this.y], this.w, this.h, "#BDBDFF");
-			draw(this.x, this.y, true);
-		},
-		onblur: function() {
-			rect(foreground, [this.x - .015, this.y - .015], this.w + .03, this.h + .03, "white");
-			draw(this.x, this.y, false);
+		x: dest[0], y: dest[1], width: width, height: height,
+		draw: function(x, y, hover) {
+			if(hover) {
+				rect(foreground, [x, y], width, height, "#bdbdff");
+			}
+			draw_content(x, y, hover);
 		},
 		onclick: onclick
 	};
 }
 
-toolbar.listeners.push(
-	//Back to menú
-	rounded_button([11.2, 0.125], 1.4, .5, function(x, y) {
+toolbar.entities.push(
+	//QUIT button
+	rounded_button_entity(function(x, y) {
 		arrow(foreground, -1, [x + .1, y + .1], 10, "black", 1.5);
 		text(foreground, "QUIT", [x + .535, y + .135], "black", 10);
-	}, function() {
-		toolbar.load_menu = true; //we delay the call to menu.load()
-								  //until we're done with this frame
-	}),
-	//Buy towers
-	rect_button([11, 1], 1, 2, function(x, y, hover) {
-		tile(foreground, [4, 1], [x, y], 1, 2);
-		tower_info(0, hover);
-	}, function() {
-		toolbar.buy_tower(0);
-	}),
-	rect_button([12, 1], 1, 2, function(x, y, hover) {
-		tile(foreground, [5, 1], [x, y], 1, 2);
-		tower_info(1, hover);
-	}, function() {
-		toolbar.buy_tower(1);
-	}),
-	rect_button([13, 1], 1, 2, function(x, y, hover) {
-		tile(foreground, [6, 1], [x, y], 1, 2);
-		tower_info(2, hover);
-	}, function() {
-		toolbar.buy_tower(2);
-	})
+	}, [11.2, 0.125], 1.4, .5, 6, menu.load)
 );
+	//TOWERS buy
+[0,1,2].forEach(function(n) {
+	toolbar.entities.push(
+		rectangular_button(function(x, y, hover) {
+			//displays tower info
+			if(hover) {
+				var tower = towers[n];
+				icon_text(foreground, [1, 0], tower.price, [14.0625, 1.25], game.gold >= tower.price ? "black" : "red");
+				icon_text(foreground, [0, 0], tower.load_time, [14.0625, 1.75], "black");
+				icon_text(foreground, [3, 0], tower.range, [14.0625, 2.25]);
+			}
+			//draws tower
+			tile(foreground, [4 + n, 1], [x, y], 1, 2);
+		}, [11 + n, 1], 1, 2, function() {
+			toolbar.buy_tower(n);
+		})
+	);
+});
 
 toolbar.buy_tower = function(n) {
 	var tower = towers[n];
@@ -673,19 +637,6 @@ toolbar.buy_tower = function(n) {
 		toolbar.buying_tower = undefined;
 	}
 };
-
-function tower_info(n, hover) {
-	if(hover) {
-		toolbar.tower_info_hover = true;
-		rect(foreground, [14, 1], 1, 2, "white");
-		var tower = towers[n];
-		icon_text(foreground, [1, 0], tower.price, [14.0625, 1.25], "black");
-		icon_text(foreground, [0, 0], tower.load_time, [14.0625, 1.75]);
-		icon_text(foreground, [3, 0], tower.range, [14.0625, 2.25]);
-	} else {
-		toolbar.tower_info_blur = true;
-	}
-}
 
 /*=====  End of BARRA DE HERRAMIENTAS DEL JUEGO  ======*/
 
